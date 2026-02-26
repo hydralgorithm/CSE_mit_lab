defce1d5d8b45ae9cd3fb05aa0a410941fb80a74
hydralgorithm <hydralgorithm@gmail.com>
2026-02-26 10:24:50 +0530

    naturalSum

---
+++ SEM2/oops_lab/pract/src/naturalSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class naturalSum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of natural numbers to find sum of: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            sum += i;
+        }
+        System.out.println("Sum: "+sum);
+    }    
+}
