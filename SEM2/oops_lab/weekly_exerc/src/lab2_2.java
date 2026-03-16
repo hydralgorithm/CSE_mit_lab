f54f433a70bd5d00d5c45cf7ea9768c73e6e6024
hydralgorithm <hydralgorithm@gmail.com>
2026-03-17 02:44:17 +0530

    year of passing

---
+++ SEM2/oops_lab/weekly_exerc/src/lab2_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class lab2_2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 9-digit registration number: ");
+        String n = sc.nextLine();
+        if (n.length() < 9) {
+            System.out.println("Invalid registration number.");
+            return;
+        }
+        String y = n.substring(0, 2);
+        int year = 2000 + Integer.parseInt(y);
+        System.out.println("Year of joining: " + year);
+        
+    }
+}
