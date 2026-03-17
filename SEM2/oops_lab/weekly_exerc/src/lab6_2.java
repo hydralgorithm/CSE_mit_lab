56cbe2b5a899b8a645a4116cc45d31d976e3dc69
hydralgorithm <hydralgorithm@gmail.com>
2026-03-18 02:35:01 +0530

    upd

---
+++ SEM2/oops_lab/weekly_exerc/src/lab6_2.java
@@ -0,0 +1,14 @@
+class Employee{
+    String name;
+    Employee(String n){
+        this.name = n;
+    }
+    void formatEmployeeName(){
+        
+    }
+}
+public class lab6_2 {
+    public static void main(String[] args){
+
+    }
+}
