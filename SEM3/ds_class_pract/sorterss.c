753ff1693b4b45f42eb4efd826ccc07be83f5080
hydralgorithm <hydralgorithm@gmail.com>
2026-09-17 18:29:49 +0530

    merge sort ready

--- SEM3/ds_class_pract/sorterss.c
+++ SEM3/ds_class_pract/sorterss.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 void bubSort(int arr[], int n){
     for(int i=0; i<n; i++){
         for(int j=0; j<n-i-1; j++){
@@ -49,6 +49,50 @@ void insertSort(int arr[], int n){
     }
     printf("\n");
 }
+void merge(int arr[], int l, int m, int r){
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int *L = malloc(n1 * sizeof(int));
+    int *R = malloc(n2 * sizeof(int));
+    for(int x=0; x<n1; x++){
+        L[x] = arr[l + x];
+    }
+    for(int x=0; x<n2; x++){
+        R[x] = arr[m + 1 + x];
+    }
+    int i=0, j=0, k=l;
+    while(i<n1 && j<n2){
+        if(L[i] <= R[j]){
+            arr[k] = L[i];
+            i++;
+        } else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+    free(L);
+    free(R);
+}
+void mergeSort(int arr[], int l, int r){
+    if(l<r){
+        int m = l + (r-l)/2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
+}
+
 int main(){
     int n;
     printf("Enter n: ");
@@ -78,8 +122,11 @@ int main(){
                 insertSort(arr,n);
                 break;
             case 4:
-            
-                break;
+                mergeSort(arr, 0, n-1); 
+                for(int i=0; i<n; i++)
+                    printf("%d ", arr[i]);
+                printf("\n");
+                break;        
             case 5:
 
                 break;
