537e9739f151251029a32aede4b460d8e1a46da3
hydralgorithm <hydralgorithm@gmail.com>
2026-09-17 17:39:42 +0530

    Insertion sort

--- SEM3/ds_class_pract/sorterss.c
+++ SEM3/ds_class_pract/sorterss.c
@@ -33,6 +33,22 @@ void selSort(int arr[], int n){
     }
     printf("\n");
 }
+void insertSort(int arr[], int n){
+    for(int i=1; i<n; i++){
+        int key = arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j=j-1;
+        }
+        arr[j+1] = key;
+    }
+    // Display
+    for(int i=0; i<n; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
 int main(){
     int n;
     printf("Enter n: ");
@@ -59,7 +75,7 @@ int main(){
                 selSort(arr,n);
                 break;
             case 3:
-
+                insertSort(arr,n);
                 break;
             case 4:
             
